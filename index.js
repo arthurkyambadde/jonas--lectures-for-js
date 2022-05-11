c41b37869da51b904891747ac5bb3fa1313a8251
iamarthur <arthurkyambadde9@gmail.com>
2022-05-11 21:56:44 +0300

    push

--- index.js
+++ index.js
@@ -144,7 +144,7 @@
 
 // const calcTip = function (billValue) {
 //   return billValue >= 50 && billValue <= 300
-//     ? billValue * 0.15
+// ? billValue * 0.15
 //     : billValue * 0.2;
 // };
 // const bills = [125, 555, 44];
@@ -219,32 +219,175 @@
 
 // // 45min
 
-const jonas = {
-  firstName: "jonas",
-  lastName: "schmedtmann",
-  middleName: "kamagu",
-  birthYeah: 1991,
-  job: "teacher",
-  friends: ["michael", "peter", "steven"],
-  hasDriverLicense: true,
+// const jonas = {
+//   firstName: "jonas",
+//   lastName: "schmedtmann",
+//   middleName: "kamagu",
+//   birthYeah: 1991,
+//   job: "teacher",
+//   friends: ["michael", "peter", "steven"],
+//   hasDriverLicense: true,
 
-  // calcAge: function (birthYeah) {
-  //   // console.log(this);
-  //   return 2037 - this.birthYeah;
-  // },
+//   // calcAge: function (birthYeah) {
+//   //   // console.log(this);
+//   //   return 2037 - this.birthYeah;
+//   // },
 
-  calcAge: function () {
-    this.age = 2017 - this.birthYeah;
-    return this.age;
-  },
+//   calcAge: function () {
+//     this.age = 2017 - this.birthYeah;
+//     return this.age;
+//   },
+// };
+
+// console.log(jonas);
+// console.log(jonas.calcAge(1991));
+// console.log(jonas["calcAge"](1991));
+
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI;
+//   },
+// };
+
+// mark.calcBMI();
+// console.log(mark.BMI);
+
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//   },
+// };
+
+// john.calcBMI();
+// console.log(john.BMI);
+
+// // LOOPS
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`lifting weights repetition ${rep}`);
+// }
+
+// FOR LOOPS
+// for(initial value; logical condition; update the counter after each iteration )
+
+// const jonas = [
+//   "jonas",
+//   "schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["michel", "peter", "steven"],
+//   true,
+// ];
+
+// const types = [];
+
+// // for (let i = 0; 1 < 5; i++) {
+// //   console.log(jonas[i]);
+// // }
+// for (let i = 0; i < jonas.length; i++) {
+//   console.log(jonas[i], typeof jonas[i]);
+
+//   // adding an element to the jonas array called types
+
+//   types.push(typeof jonas[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
+
+// // const years2 = [2040, 2070, 2050, 2060];
+
+// // const ages2 = [];
+
+// // for (let i = 0; i < years2.length; i++) {
+// //   ages2.push(2090 - years[i]);
+// // }
+
+// // console.log(ages2);
+// // continue and break statments
+
+// console.log("------only strings------");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== "string") continue;
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log("------break when number is found------");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] == "number") break;
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// const jonas = [
+//   "jonas",
+//   "schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["michel", "peter", "steven"],
+//   true,
+// ];
+
+// // looping backwards
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//   console.log(jonas[i]);
+// }
+
+// // creating a loop inside a loop
+
+// for (let exercise = 1; exercise <= 3; exercise++) {
+//   console.log(`---starting exercise ${exercise}`);
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise} Lifting weight repetition ${rep}`);
+//   }
+// }
+
+// while loops
+let rep = 1;
+while (rep <= 10) {
+  console.log(`lifting weights repetition ${rep}`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+while (dice !== 6) {
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice == 6) {
+    console.log(`loop is about to end.....`);
+  }
+}
+
+// CHALLENGE
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-console.log(jonas);
-console.log(jonas.calcAge(1991));
-console.log(jonas["calcAge"](1991));
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-console.log(jonas.age);
-console.log(jonas.age);
-console.log(jonas.age);
-console.log(jonas.age);
-console.log(jonas.age);
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {}
